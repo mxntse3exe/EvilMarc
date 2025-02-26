c3d601775431a7b9b95434ff38e9448adbcebe04
mxntse3exe <163748602+mxntse3exe@users.noreply.github.com>
2025-02-26 15:36:51 +0100

    compartir fitxers

--- web/compartir_arxiu.php
+++ web/compartir_arxiu.php
@@ -13,14 +13,15 @@ $password = "T5Dk!xq";
 $db = "evilmarc";
 
 $conexion = mysqli_connect($servidor, $usuario, $password, $db);
-
 if (!$conexion) {
     die("Error al connectar amb la base de dades: " . mysqli_connect_error());
 }
 
 // Obtenir dades del formulari
 $ruta = $_POST['ruta_arxiu'];
+$usuaris_seleccionats = $_POST['usuaris'] ?? [];
 $departaments_seleccionats = $_POST['departaments'] ?? [];
+$url = $_POST['current_dir'];
 
 // Obtenir l'ID de l'usuari
 if (!isset($_SESSION['id_usu'])) {
@@ -43,14 +44,48 @@ if (mysqli_num_rows($result_id_arxiu) == 0) {
 $row = mysqli_fetch_assoc($result_id_arxiu);
 $id_arxiu = $row['id_arxiu'];
 
+
+// ==========================
+// 1. Gestionar usuaris
+// ==========================
+
+// Obtenir usuaris que ja tenen accés
+$sql_usuaris_compartits = "select id_destinatari from ARXIUS_COMPARTITS_USUARIS where id_arxiu = '$id_arxiu'";
+$result_usuaris_compartits = mysqli_query($conexion, $sql_usuaris_compartits);
+$usuaris_compartits = [];
+while ($row = mysqli_fetch_assoc($result_usuaris_compartits)) {
+    $usuaris_compartits[] = $row['id_destinatari'];
+}
+
+// Afegir noves comparticions
+foreach ($usuaris_seleccionats as $id_destinatari) {
+    if (!in_array($id_destinatari, $usuaris_compartits)) {
+        $sql_insert = "INSERT INTO ARXIUS_COMPARTITS_USUARIS (id_propietari, id_destinatari, id_arxiu) 
+                       VALUES ('$usuari', '$id_destinatari', '$id_arxiu')";
+        if (!mysqli_query($conexion, $sql_insert)) {
+            die("Error en la inserció d'usuaris: " . mysqli_error($conexion));
+        }
+    }
+}
+
+// Eliminar comparticions que ja no estan seleccionades
+foreach ($usuaris_compartits as $id_destinatari) {
+    if (!in_array($id_destinatari, $usuaris_seleccionats)) {
+        $sql_delete = "DELETE FROM ARXIUS_COMPARTITS_USUARIS 
+                       WHERE id_arxiu = '$id_arxiu' AND id_destinatari = '$id_destinatari'";
+        if (!mysqli_query($conexion, $sql_delete)) {
+            die("Error en l'eliminació d'usuaris: " . mysqli_error($conexion));
+        }
+    }
+}
+
+// ==========================
+// 2. Gestionar departaments
+// ==========================
+
 // Obtenir departaments que ja tenen accés
 $sql_departaments_compartits = "select id_dep from ARXIUS_COMPARTITS_DEPARTAMENTS where id_arxiu = '$id_arxiu'";
 $result_departaments_compartits = mysqli_query($conexion, $sql_departaments_compartits);
-
-if (!$result_departaments_compartits) {
-    die("Error en la consulta: " . mysqli_error($conexion));
-}
-
 $departaments_compartits = [];
 while ($row = mysqli_fetch_assoc($result_departaments_compartits)) {
     $departaments_compartits[] = $row['id_dep'];
@@ -62,7 +97,7 @@ foreach ($departaments_seleccionats as $id_dep) {
         $sql_insert = "INSERT INTO ARXIUS_COMPARTITS_DEPARTAMENTS (id_propietari, id_dep, id_arxiu) 
                        VALUES ('$usuari', '$id_dep', '$id_arxiu')";
         if (!mysqli_query($conexion, $sql_insert)) {
-            die("Error en la inserció: " . mysqli_error($conexion));
+            die("Error en la inserció de departaments: " . mysqli_error($conexion));
         }
     }
 }
@@ -73,12 +108,12 @@ foreach ($departaments_compartits as $id_dep) {
         $sql_delete = "DELETE FROM ARXIUS_COMPARTITS_DEPARTAMENTS 
                        WHERE id_arxiu = '$id_arxiu' AND id_dep = '$id_dep'";
         if (!mysqli_query($conexion, $sql_delete)) {
-            die("Error en l'eliminació: " . mysqli_error($conexion));
+            die("Error en l'eliminació de departaments: " . mysqli_error($conexion));
         }
     }
 }
 
 // Redireccionar amb missatge d'èxit
-header("Location: pujar_fitxers.php");
+header("Location: pujar_fitxers.php?dir=".urlencode($url));
 exit();
 ?>
--- web/panell_usuari.php
+++ web/panell_usuari.php
@@ -114,9 +114,9 @@
                     </div>
 
                     <div class="contingut_panell">
-                        <a href="pujar_fitxers" class="link_panell"><div class="botons_panell"><span>Pujar i escanejar arxius</span></div></a>
-                        <a href="" class="link_panell"><div class="botons_panell"><span>Els meus arxius</span></div></a>
-                        <a href="" class="link_panell"><div class="botons_panell"><span>Arxius compartits amb mi</span></div></a>
+                        <a href="pujar_fitxers" class="link_panell"><div class="botons_panell"><span>Els meus arxius</span></div></a>
+                        <a href="compartits" class="link_panell"><div class="botons_panell"><span>Arxius compartits amb mi</span></div></a>
+                        <a href="" class="link_panell"><div class="botons_panell"><span>Missatges</span></div></a>
                         <a href="" class="link_panell"><div class="botons_panell"><span>Registre d'arxius pujats</span></div></a>
                         <a href="compte" class="link_panell">
                             <div class="botons_panell">
